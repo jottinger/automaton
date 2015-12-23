637db64b7bd9425c3d04fa2a06a5acd93d90b3d4
Joseph B. Ottinger <joseph@perahealth.com>
2015-12-23 14:57:28 -0500

    fixing formatting

--- src/main/python/life2d.py
+++ src/main/python/life2d.py
@@ -1,44 +1,44 @@
 class Automaton2D:
-	def __init__(self, l=64, p=30):
-		self.length = l
-		self.pattern = p
-		self.data = [0] * self.length
-		self.data[self.length / 2] = 1
+    def __init__(self, l=64, p=30):
+        self.length = l
+        self.pattern = p
+        self.data = [0] * self.length
+        self.data[self.length / 2] = 1
 
-	def octet(self, offset):
-		value = 0
-		for j in self.data[max(0, offset - 1):min(self.length, offset + 2)]:
-			value = value * 2 + j
-		return value
+    def octet(self, offset):
+        value = 0
+        for j in self.data[max(0, offset - 1):min(self.length, offset + 2)]:
+            value = value * 2 + j
+        return value
 
-	def cellvalue(self, octet):
-		value = 0
-		if octet > 1:
-			value = (self.pattern >> (octet - 1)) & 1
-		return value
+    def cellvalue(self, octet):
+        value = 0
+        if octet > 1:
+            value = (self.pattern >> (octet - 1)) & 1
+        return value
 
-	def formatted(self):
-		output = ""
-		for j in range(0, len(self.data)):
-			if self.data[j] == 1:
-				output += "*"
-			else:
-				output += " "
-		return output
+    def formatted(self):
+        output = ""
+        for j in range(0, len(self.data)):
+            if self.data[j] == 1:
+                output += "*"
+            else:
+                output += " "
+        return output
 
-	def nextgeneration(self):
-		gen = [0] * self.length
-		for j in range(1, len(self.data)):
-			if self.cellvalue(self.octet(j)) == 1:
-				gen[j] = 1
-		self.data = gen
+    def nextgeneration(self):
+        gen = [0] * self.length
+        for j in range(1, len(self.data)):
+            if self.cellvalue(self.octet(j)) == 1:
+                gen[j] = 1
+        self.data = gen
 
-	def dump(self):
-		print "length=", self.length
-		print "pattern=", self.pattern
+    def dump(self):
+        print "length=", self.length
+        print "pattern=", self.pattern
 
 
 d = Automaton2D()
 for i in range(0, 30):
-	d.nextgeneration()
-	print d.formatted()
+    d.nextgeneration()
+    print d.formatted()
